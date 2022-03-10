55a6a58b51413d753d8edb504f3b87f12b995ba9
Rynah-Crynah <swabrinyusufshamun@gmail.com>
2022-03-10 08:56:34 +0300

    Kotlin assessment

---
+++ src/main.kt
@@ -0,0 +1,40 @@
+fun main (){
+    Modulus(225,13)
+ Identity("Hi, my name is Swabra, ")
+    myCount("Swabrina")
+
+
+}
+
+fun Institute(name: String){
+   var w = "codeHive"
+    println()
+
+}
+
+fun Modulus(number1: Int, number2: Int): Int{
+    var w = (number1 % number2)
+    println(w)
+    return w
+
+}
+
+fun Identity(name: String): String{
+    var u = ("$name and am 21 years old.")
+    println(u)
+    return u
+}
+
+fun myCount(name: String): String{
+    var t = name
+    println(name.length)
+    return t
+
+
+
+}
+
+
+
+
+
